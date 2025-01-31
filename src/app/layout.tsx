e43746045ca5f8e47b39b40529768fe0843fcfa3
Le Quoc Uy <lequocuyit@gmail.com>
2025-01-31 21:59:48 +0700

    feat: update metadata for Cifarm landing page with enhanced SEO and social sharing information

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,52 @@
 import type { Metadata } from "next"
-import "./globals.css"
 import { dinosaur, miSans } from "./fonts"
+import "./globals.css"
 
 export const metadata: Metadata = {
-    title: "Cifarm Landing Page",
-    description: "Cifarm Landing Page",
+    title: "Cifarm - Farm-to-earn on Telegram!",
+    description: "Join Cifarm, farm, help, visit, and even steal from other players while earning airdropped tokens!",
+    
+    robots: "index, follow",
+
+    openGraph: {
+        title: "Cifarm - Farm-to-earn on Telegram!",
+        description: "Join Cifarm, farm, help, visit, and even steal from other players while earning airdropped tokens!",
+        url: "https://landing-page.cifarm.starci.io",
+        siteName: "Cifarm",
+        images: [
+            {
+                url: "/backgrounds/desktop.png",
+                width: 1200,
+                height: 630,
+                alt: "Cifarm Landing Page",
+            },
+        ],
+        type: "website",
+    },
+
+    twitter: {
+        card: "summary_large_image",
+        title: "Cifarm - Farm-to-earn on Telegram!",
+        description: "Join Cifarm, Farm, help, visit, and even steal from other players while earning airdropped tokens",
+        images: ["/backgrounds/desktop.png"],
+        creator: "@CifarmOfficial",
+    },
+
+    icons: {
+        icon: "/favicon.ico",
+        apple: "/backgrounds/desktop.png",
+    },
+    themeColor: "#553320",
 }
 
 export default function RootLayout({
     children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
     return (
         <html lang="en">
-            <body
-                className={`${miSans.variable} ${dinosaur.variable}  antialiased`}
-            >
+            <body className={`${miSans.variable} ${dinosaur.variable} antialiased`}>
                 {children}
             </body>
         </html>
