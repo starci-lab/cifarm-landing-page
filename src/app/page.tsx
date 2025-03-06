aa9dd114e1d7b101bbf3f286a1bccadc4ec089cc
Le Quoc Uy <lequocuyit@gmail.com>
2025-03-06 20:56:46 +0700

    sad

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,12 +27,19 @@ const Page: FC = () => {
                             width={1920}
                             height={1080}
                             alt="Hero background"
-                            className="object-cover w-full h-full object-bottom z-40-"
+                            className="hidden lg:block object-cover w-full h-full object-bottom z-40-"
+                        />
+                        {/* Mobile */}
+                        <Image
+                            src="/backgrounds/about_mobie.jpg"
+                            width={1080}
+                            height={1920}
+                            alt="Hero background"
+                            className="lg:hidden object-cover w-full h-full"
                         />
                         <div
                             className="absolute inset-0 bg-gradient-to-b from-[#E1F4FD] from-[0%] to-[10%] to-transparent"
                         />
-
                         <div
                             className="absolute inset-0 bg-gradient-to-t from-yellow from-[0%] to-[10%] to-transparent"
                         />
